e977eb7a3316a87db63052106b9a2c5e841e5473
Elmurzayev Rahim <rahim.elmurzayev@stduent.ap.be>
2020-04-30 13:42:58 +0200

    CRUD actions voor TechniekenController

--- JudoTechnieken/JudoTechnieken/Controllers/TechniekenController.cs
+++ JudoTechnieken/JudoTechnieken/Controllers/TechniekenController.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Threading.Tasks;
+using JudoTechnieken.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JudoTechnieken.Controllers
 {   
     [Route("api/v1/technieken")]
     public class TechniekenController : Controller
-    {   
-        [HttpGet]
-        public IActionResult Index()
+    {
+        private readonly JudoContext context;
+        
+        public TechniekenController(JudoContext context)
         {
-            return View();
+            this.context = context;
         }
+        [HttpGet]
+        public List<Technieken> GetAllTechnieken()
+        {
+            return context.Technieken.ToList();
+        }
+
+        [HttpPost]
+        public IActionResult CreateTechniek([FromBody] Technieken newTechniek)
+        {
+            context.Technieken.Add(newTechniek);
+            context.SaveChanges();
+            return Created("", newTechniek);
+        }
+
+
     }
 }
